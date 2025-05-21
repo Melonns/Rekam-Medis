d5fa3acd5a77608a455b6784899fbb76763af288
Melon <alvianprasetio24@gmail.com>
2025-05-21 17:01:55 +0700

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,13 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-<<<<<<< Updated upstream
-      '/api/data': { // Ubah path prefix proxy menjadi /api/data
-        target: 'server-production-bb65.up.railway.app',
-=======
       '/api': {
         target: 'http://localhost:3000',
->>>>>>> Stashed changes
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // ✅ HANYA ini yang perlu
       },
