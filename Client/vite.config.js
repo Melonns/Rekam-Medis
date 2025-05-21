e7cc51fa13f566b781d95022ad173e6adfd2ef16
Melon <alvianprasetio24@gmail.com>
2025-05-21 17:48:03 +0700

    fix link

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'serverrekammedis',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // ✅ HANYA ini yang perlu
       },
